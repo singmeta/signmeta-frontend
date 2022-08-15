2bf246138bfa742080f8241b37681c171579dacf
jinhobyeong <jhb_0921@naver.com>
2022-08-16 00:22:44 +0900

    chore : fix

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -10,7 +10,7 @@ function MainPage() {
   return (
     <section className="h-screen ">
       <div className="flex justify-center ">
-        <div className="grid grid-cols-6 gap-3 mt-40 h-full bg-no-repeat bg-singmetaBack flex justify-center rounded p-6 m-10">
+        <div className="grid grid-cols-6 gap-3 mt-40 h-full max-w-7xl bg-no-repeat bg-singmetaBack flex justify-center rounded p-6 m-10">
           <div className="col-start-1 col-span-2 ... flex items-center ml-3">
             <Profile1 />
             <div className="ml-2">
@@ -20,17 +20,30 @@ function MainPage() {
               </p>
             </div>
           </div>
-          <div className="col-end-7 col-span-1 ...">exit</div>
-          <div className="col-start-1 col-span-1 ...">playlist</div>
-          <div className="col-end-5 row-span-2 col-span-2 ...">character</div>
-          <div className="col-end-7 col-span-1 ...">chart</div>
-          <div className="col-start-1 col-span-1 ...">mypage</div>
-          <div className="col-start-1 col-span-1 ...">empty</div>
           <div className="col-end-7 col-span-1 ...">
-            <strong>호옹이</strong>
-            <strong>호옹이</strong>
-            <strong>호옹이</strong>
-            <strong>호옹이</strong>
+            <img src="https://i.imgur.com/6eE5nJm.png" alt="" />
+            exit
+          </div>
+          <div className="col-start-1 col-span-1 ...">
+            <img src="https://i.imgur.com/xvXJoPm.png" alt="" />
+            playlist
+          </div>
+          <div className="col-end-5 row-span-2 col-span-2 ...">
+            <img src="https://i.imgur.com/WkuQGWl.png" alt="" />
+            character
+          </div>
+          <div className="col-end-7 col-span-1 ...">
+            <img src="https://i.imgur.com/YEH2xPE.png" alt="" />
+            chart
+          </div>
+          <div className="col-start-1 col-span-1 ...">
+            <img src="https://i.imgur.com/oI7F4hZ.png" alt="" />
+            mypage
+          </div>
+          <div className="col-start-1 col-span-1 ..."></div>
+          <div className="col-end-7 col-span-1 ...">
+            <img src="https://i.imgur.com/J73mp5N.png" alt="" />
+            <strong>room</strong>
           </div>
         </div>
       </div>
