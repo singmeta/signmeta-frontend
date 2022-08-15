727680330f9629600f4c4425b924ac8a84755ba5
jinhobyeong <jhb_0921@naver.com>
2022-08-16 00:45:04 +0900

    chore : mainpage ui finish

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -9,41 +9,43 @@ function MainPage() {
 
   return (
     <section className="h-screen ">
-      <div className="flex justify-center ">
-        <div className="grid grid-cols-6 gap-3 mt-40 h-full max-w-7xl bg-no-repeat bg-singmetaBack flex justify-center rounded p-6 m-10">
-          <div className="col-start-1 col-span-2 ... flex items-center ml-3">
-            <Profile1 />
-            <div className="ml-2">
-              <strong>호옹이</strong>
-              <p>
-                <span className="text-white text-sm">프로필 수정하기</span>
-              </p>
+      <div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
+        <div className="xl:w-7/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
+          <div className="grid grid-cols-6 gap-3 h-full max-w-7xl bg-no-repeat bg-singmetaBack flex justify-center rounded p-6 m-10">
+            <div className="col-start-1 col-span-2 ... flex items-center ml-7 m-auto">
+              <Profile1 />
+              <div className="ml-3">
+                <strong>호옹이</strong>
+                <p>
+                  <span className="text-white text-sm">프로필 수정하기</span>
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="col-end-7 col-span-1 ...">
-            <img src="https://i.imgur.com/6eE5nJm.png" alt="" />
-            exit
-          </div>
-          <div className="col-start-1 col-span-1 ...">
-            <img src="https://i.imgur.com/xvXJoPm.png" alt="" />
-            playlist
-          </div>
-          <div className="col-end-5 row-span-2 col-span-2 ...">
-            <img src="https://i.imgur.com/WkuQGWl.png" alt="" />
-            character
-          </div>
-          <div className="col-end-7 col-span-1 ...">
-            <img src="https://i.imgur.com/YEH2xPE.png" alt="" />
-            chart
-          </div>
-          <div className="col-start-1 col-span-1 ...">
-            <img src="https://i.imgur.com/oI7F4hZ.png" alt="" />
-            mypage
-          </div>
-          <div className="col-start-1 col-span-1 ..."></div>
-          <div className="col-end-7 col-span-1 ...">
-            <img src="https://i.imgur.com/J73mp5N.png" alt="" />
-            <strong>room</strong>
+            <button className="col-end-7 col-span-1 ... m-auto  text-white">
+              <img src="https://i.imgur.com/6eE5nJm.png" alt="exit" />
+              exit
+            </button>
+            <button className="col-start-1 col-span-1 ... m-auto text-white">
+              <img src="https://i.imgur.com/xvXJoPm.png" alt="" />
+              playlist
+            </button>
+            <button className="col-end-5 row-span-2 col-span-2 ... m-auto">
+              <img src="https://i.imgur.com/WkuQGWl.png" alt="character" />
+            </button>
+            <button className="col-end-7 col-span-1 ... m-auto  text-white">
+              <img src="https://i.imgur.com/YEH2xPE.png" alt="" />
+              <span>chart</span>
+            </button>
+            <button className="col-start-1 col-span-1 ... m-auto text-white">
+              <img src="https://i.imgur.com/oI7F4hZ.png" alt="" />
+              mypage
+            </button>
+            <button className="col-start-1 col-span-1 ..."></button>
+            <button className="col-end-7 col-span-1 ... m-auto">
+              <a href="/signup" className="text-neutral-400">
+                <img src="https://i.imgur.com/J73mp5N.png" alt="room" />
+              </a>
+            </button>
           </div>
         </div>
       </div>
