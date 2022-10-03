eaa53b9d02915da9ffe1cac7f039b9c88ecd8149
jinhobyeong <jhb_0921@naver.com>
2022-10-04 02:21:39 +0900

    fix : search modal update

--- src/components/modal/SearchModal.jsx
+++ src/components/modal/SearchModal.jsx
@@ -1,10 +1,24 @@
 import ChartDetail from "components/chartDetail";
-import React, { useState } from "react";
-import Modal from "../modal/modal.css";
+import { Swiper, SwiperSlide } from "swiper/react"; // basic
+import SwiperCore, { Navigation, Scrollbar, Pagination } from "swiper";
+import "swiper/css"; //basic
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const SearchModal = (props) => {
   // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
   const { open, close, header } = props;
+
+  SwiperCore.use([Navigation, Pagination, Scrollbar]);
+
+  const settings = {
+    spaceBetween: 10,
+    navigation: {},
+    pagination: { clickable: true },
+    scrollbar: { draggable: true, el: null },
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+  };
 
   return (
     // 모달이 열릴때 openModal 클래스가 생성된다.
@@ -18,18 +32,22 @@ const SearchModal = (props) => {
             </button>
           </header>
           <main className="flex justify-center">
-            {Object.values(header)?.map((item, index) => (
-              <ChartDetail
-                title={item.title}
-                likes={item.likes}
-                music_id={item._id}
-                music_url={item.record_url}
-                user_nickname={item.user_nickname}
-                ranking={index}
-                user_id={item.user_id}
-                key={index}
-              />
-            ))}
+            <Swiper {...settings} className="flex justify-center m-10">
+              {Object.values(header)?.map((item, index) => (
+                <SwiperSlide className="flex justify-center pb-10" key={index}>
+                  <ChartDetail
+                    title={item.title}
+                    likes={item.likes}
+                    music_id={item._id}
+                    music_url={item.record_url}
+                    user_nickname={item.user_nickname}
+                    ranking={index}
+                    user_id={item.user_id}
+                    key={index}
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </main>
           <footer>
             <button className="close" onClick={close}>
